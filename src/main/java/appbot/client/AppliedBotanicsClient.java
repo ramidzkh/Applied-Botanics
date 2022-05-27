912083f7ce7e4eb9a03f40f5dc82586a7a83fa32
Ramid Khan <ramidzkh@gmail.com>
2022-05-28 08:44:01 +1000

    Stop the server crashing on load

--- src/main/java/appbot/client/AppliedBotanicsClient.java
+++ src/main/java/appbot/client/AppliedBotanicsClient.java
@@ -1,5 +1,8 @@
 package appbot.client;
 
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+
+import appbot.ABItems;
 import appbot.ABMenus;
 import appbot.ae2.ManaKey;
 import appbot.ae2.ManaKeyType;
@@ -7,12 +10,17 @@ import appbot.ae2.ManaKeyType;
 import appeng.api.client.AEStackRendering;
 import appeng.client.gui.me.common.MEStorageScreen;
 import appeng.init.client.InitScreens;
+import appeng.items.storage.BasicStorageCell;
 import appeng.menu.me.common.MEStorageMenu;
 
 public interface AppliedBotanicsClient {
 
     static void initialize() {
         AEStackRendering.register(ManaKeyType.TYPE, ManaKey.class, new ManaRenderer());
+
+        for (var tier : ABItems.Tier.values()) {
+            ColorProviderRegistry.ITEM.register(BasicStorageCell::getColor, ABItems.get(tier));
+        }
 
         InitScreens.<MEStorageMenu, MEStorageScreen<MEStorageMenu>>register(ABMenus.PORTABLE_MANA_CELL_TYPE,
                 MEStorageScreen::new, "/screens/terminals/portable_mana_cell.json");
