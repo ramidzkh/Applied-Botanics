4f7327af263a23dc22b1caa7707ba7afa790c931
Ramid Khan <ramidzkh@gmail.com>
2022-07-17 12:16:03 +1000

    Colour

--- src/main/java/appbot/client/AppliedBotanicsClient.java
+++ src/main/java/appbot/client/AppliedBotanicsClient.java
@@ -1,11 +1,18 @@
 package appbot.client;
 
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.util.Mth;
+import net.minecraft.world.item.DyeColor;
 
+import appbot.ABBlocks;
 import appbot.ABItems;
 import appbot.ABMenus;
 import appbot.ae2.ManaKey;
 import appbot.ae2.ManaKeyType;
+import vazkii.botania.client.core.handler.ClientTickHandler;
+import vazkii.botania.common.block.tile.mana.TilePool;
+import vazkii.botania.common.helper.ColorHelper;
+import vazkii.botania.common.helper.MathHelper;
 
 import appeng.api.client.AEStackRendering;
 import appeng.client.gui.me.common.MEStorageScreen;
@@ -24,5 +31,33 @@ public interface AppliedBotanicsClient {
 
         InitScreens.<MEStorageMenu, MEStorageScreen<MEStorageMenu>>register(ABMenus.PORTABLE_MANA_CELL_TYPE,
                 MEStorageScreen::new, "/screens/terminals/portable_mana_cell.json");
+
+        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> {
+            if (tintIndex != 0) {
+                return -1;
+            }
+
+            int color = ColorHelper.getColorValue(DyeColor.WHITE);
+
+            if (world != null && pos != null) {
+                if (world.getBlockEntity(pos)instanceof TilePool pool) {
+                    color = ColorHelper.getColorValue(pool.getColor());
+                }
+            }
+
+            float time = ClientTickHandler.ticksInGame + ClientTickHandler.partialTicks;
+            int fabulousColor = Mth.hsvToRgb(240 + 20 * Mth.sin(time / 200), 0.6F, 1F);
+            return MathHelper.multiplyColor(fabulousColor, color);
+        }, ABBlocks.FLUIX_MANA_POOL);
+
+        ColorProviderRegistry.ITEM.register((itemStack, tintIndex) -> {
+            if (tintIndex != 0) {
+                return -1;
+            }
+
+            int color = ColorHelper.getColorValue(DyeColor.WHITE);
+            int fabulousColor = Mth.hsvToRgb(250, 0.6F, 1F);
+            return MathHelper.multiplyColor(fabulousColor, color);
+        }, ABItems.FLUIX_MANA_POOL);
     }
 }
