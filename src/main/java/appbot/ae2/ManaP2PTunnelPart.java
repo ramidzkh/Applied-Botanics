3a7cfd3afbcf173774035bad275daa93fa91c443
Ramid Khan <ramidzkh@gmail.com>
2025-11-27 22:43:41 +1100

    Keep burst rotation and direction, give part orientation

--- src/main/java/appbot/ae2/ManaP2PTunnelPart.java
+++ src/main/java/appbot/ae2/ManaP2PTunnelPart.java
@@ -3,13 +3,18 @@ package appbot.ae2;
 import java.util.List;
 
 import org.jetbrains.annotations.UnknownNullability;
+import org.joml.Matrix3d;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.FastColor;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 
 import appbot.AppliedBotanics;
@@ -20,35 +25,23 @@ import vazkii.botania.common.handler.BotaniaSounds;
 
 import appeng.api.config.Actionable;
 import appeng.api.config.PowerMultiplier;
+import appeng.api.orientation.BlockOrientation;
 import appeng.api.parts.IPartItem;
 import appeng.api.parts.IPartModel;
 import appeng.core.AEConfig;
 import appeng.parts.AEBasePart;
 import appeng.parts.p2p.P2PModels;
 import appeng.parts.p2p.P2PTunnelPart;
+import appeng.util.InteractionUtil;
 
-public class ManaP2PTunnelPart extends P2PTunnelPart<ManaP2PTunnelPart> implements ManaReceiver {
+public class ManaP2PTunnelPart extends P2PTunnelPart<ManaP2PTunnelPart> implements ManaReceiver, SafeMana {
 
     private static final double MANA_RETAINED = 0.95;
     private static final int EXTRA_TICKS_EXISTED = 1;
 
-    private static final float[] ROTX, ROTY;
     private static final P2PModels MODELS = new P2PModels(AppliedBotanics.id("part/mana_p2p_tunnel"));
 
-    static {
-        ROTX = new float[6];
-        ROTY = new float[6];
-
-        ROTY[Direction.DOWN.get3DDataValue()] = -90;
-        ROTY[Direction.UP.get3DDataValue()] = 90;
-
-        ROTX[Direction.DOWN.get3DDataValue()] = -90;
-        ROTX[Direction.UP.get3DDataValue()] = -90;
-        ROTX[Direction.NORTH.get3DDataValue()] = 0;
-        ROTX[Direction.SOUTH.get3DDataValue()] = -180;
-        ROTX[Direction.WEST.get3DDataValue()] = -90;
-        ROTX[Direction.EAST.get3DDataValue()] = -270;
-    }
+    private byte spin;
 
     public ManaP2PTunnelPart(IPartItem<?> partItem) {
         super(partItem);
@@ -66,6 +59,41 @@ public class ManaP2PTunnelPart extends P2PTunnelPart<ManaP2PTunnelPart> implemen
     @Override
     public IPartModel getStaticModels() {
         return MODELS.getModel(this.isPowered(), this.isActive());
+    }
+
+    @Override
+    public void readFromNBT(CompoundTag data, HolderLookup.Provider registries) {
+        super.readFromNBT(data, registries);
+        spin = data.getByte("spin");
+    }
+
+    @Override
+    public void writeToNBT(CompoundTag data, HolderLookup.Provider registries) {
+        super.writeToNBT(data, registries);
+        data.putByte("spin", spin);
+    }
+
+    @Override
+    public boolean onUseWithoutItem(Player player, Vec3 pos) {
+        if (InteractionUtil.canWrenchRotate(player.getInventory().getSelected())) {
+            if (!isClientSide()) {
+                this.spin = (byte) ((this.spin + 1) % 4);
+                this.getHost().markForUpdate();
+                this.getHost().markForSave();
+            }
+            return true;
+        } else {
+            return super.onUseWithoutItem(player, pos);
+        }
+    }
+
+    @Override
+    public void onPlacement(Player player) {
+        super.onPlacement(player);
+
+        if (getSide().getAxis() == Direction.Axis.Y) {
+            this.spin = (byte) (Mth.floor(player.getYRot() * 4F / 360F + 2.5D) & 3);
+        }
     }
 
     // <editor-fold desc="Fake ManaReceiver">
@@ -98,11 +126,21 @@ public class ManaP2PTunnelPart extends P2PTunnelPart<ManaP2PTunnelPart> implemen
     public boolean canReceiveManaFromBursts() {
         return !isFull();
     }
+
+    @Override
+    public int insert(int amount, Actionable mode) {
+        return 0;
+    }
+
+    @Override
+    public int extract(int amount, Actionable mode) {
+        return 0;
+    }
     // </editor-fold>
 
-    public void receiveManaFromBurst(ManaBurstEntity burst) {
+    public void receiveManaFromBurst(ManaBurstEntity burst, BlockHitResult hit) {
         var node = getMainNode().getNode();
-        int mana = burst.getMana();
+        var mana = burst.getMana();
 
         if (node == null || isFull() || mana <= 0) {
             return;
@@ -112,7 +150,31 @@ public class ManaP2PTunnelPart extends P2PTunnelPart<ManaP2PTunnelPart> implemen
         if (outputs.isEmpty())
             return;
 
+        var input = this;
         var output = outputs.get(getLevel().getRandom().nextInt(outputs.size()));
+
+        Vec3 moveTo, directionTo;
+        float xrot, yrot;
+
+        {
+            var sideInput = input.getSide();
+            var sideOutput = output.getSide();
+
+            var originInput = Vec3.atCenterOf(input.getBlockEntity().getBlockPos())
+                    .add(Vec3.atLowerCornerOf(sideInput.getNormal()).scale(0.5)).toVector3f();
+            var originOutput = Vec3.atCenterOf(output.getBlockEntity().getBlockPos())
+                    .add(Vec3.atLowerCornerOf(sideOutput.getNormal()).scale(0.5)).toVector3f();
+
+            var r = onb(sideOutput, output.getOrientation())
+                    .mul(onb(sideInput, input.getOrientation()).transpose());
+
+            moveTo = new Vec3(hit.getLocation().toVector3f().sub(originInput).mul(r).add(originOutput));
+            directionTo = new Vec3(burst.getDeltaMovement().toVector3f().mul(r)).scale(-1);
+
+            var xz = Math.sqrt(directionTo.x * directionTo.x + directionTo.z * directionTo.z);
+            xrot = Mth.wrapDegrees((float) -(Mth.atan2(directionTo.y, xz) * Mth.RAD_TO_DEG));
+            yrot = Mth.wrapDegrees((float) (Mth.atan2(directionTo.z, directionTo.x) * Mth.RAD_TO_DEG) - 90.0F);
+        }
 
         var costFactor = AEConfig.instance().getP2PTunnelTransportTax();
 
@@ -129,17 +191,14 @@ public class ManaP2PTunnelPart extends P2PTunnelPart<ManaP2PTunnelPart> implemen
         }
 
         var level = output.getLevel();
-        var dir = output.getSide();
-        var pos = output.getBlockEntity().getBlockPos();
 
-        int old = burst.getColor();
-        int rainbow = Mth.hsvToRgb(level.getGameTime() * 2 % 360 / 360F, 1F, 1F);
-        int newColor = FastColor.ARGB32.lerp(0.4f, old, rainbow);
+        var old = burst.getColor();
+        var rainbow = Mth.hsvToRgb(level.getGameTime() * 2 % 360 / 360F, 1F, 1F);
+        var newColor = FastColor.ARGB32.lerp(0.4f, old, rainbow);
 
         var newBurst = new ManaBurstEntity(BotaniaEntities.MANA_BURST, level);
-        var at = Vec3.atCenterOf(pos).add(Vec3.atLowerCornerOf(dir.getNormal()).scale(0.5));
-        newBurst.moveTo(at.x, at.y, at.z, ROTX[dir.get3DDataValue()], ROTY[dir.get3DDataValue()]);
-        newBurst.setDeltaMovement(ManaBurstEntity.calculateBurstVelocity(newBurst.getXRot(), newBurst.getYRot()));
+        newBurst.moveTo(moveTo.x, moveTo.y, moveTo.z, yrot, xrot);
+        newBurst.setDeltaMovement(directionTo);
         newBurst.setColor(newColor);
         newBurst.setMana(mana);
         newBurst.setStartingMana(burst.getStartingMana());
@@ -151,7 +210,28 @@ public class ManaP2PTunnelPart extends P2PTunnelPart<ManaP2PTunnelPart> implemen
         newBurst.setTicksExisted(burst.getTicksExisted() + ManaP2PTunnelPart.EXTRA_TICKS_EXISTED);
 
         level.addFreshEntity(newBurst);
-        level.playSound(null, pos, BotaniaSounds.spreaderFire, SoundSource.BLOCKS, 0.05F,
+        level.playSound(null, moveTo.x, moveTo.y, moveTo.z, BotaniaSounds.spreaderFire, SoundSource.BLOCKS, 0.05F,
                 0.7F + 0.3F * (float) Math.random());
+    }
+
+    private static Matrix3d onb(Direction a, Direction b) {
+        var m = new Matrix3d();
+        var va = a.getNormal();
+        var vb = b.getNormal();
+        var vc = va.cross(vb);
+        m.m00 = va.getX();
+        m.m01 = va.getY();
+        m.m02 = va.getZ();
+        m.m10 = vb.getX();
+        m.m11 = vb.getY();
+        m.m12 = vb.getZ();
+        m.m20 = vc.getX();
+        m.m21 = vc.getY();
+        m.m22 = vc.getZ();
+        return m;
+    }
+
+    private Direction getOrientation() {
+        return BlockOrientation.get(getSide(), spin).rotate(Direction.UP);
     }
 }
--- src/main/java/appbot/mixins/ManaBurstEntityMixin.java
+++ src/main/java/appbot/mixins/ManaBurstEntityMixin.java
@@ -1,9 +1,12 @@
 package appbot.mixins;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.phys.BlockHitResult;
 
 import appbot.ae2.ManaP2PTunnelPart;
 import vazkii.botania.api.mana.ManaReceiver;
@@ -12,11 +15,14 @@ import vazkii.botania.common.entity.ManaBurstEntity;
 @Mixin(ManaBurstEntity.class)
 public class ManaBurstEntityMixin {
 
-    @Inject(method = "onReceiverImpact", at = @At("HEAD"), cancellable = true)
-    private void onReceiverImpact(ManaReceiver receiver, CallbackInfoReturnable<Boolean> cir) {
-        if (receiver instanceof ManaP2PTunnelPart p2p) {
-            p2p.receiveManaFromBurst((ManaBurstEntity) (Object) this);
-            cir.setReturnValue(false);
+    @WrapOperation(method = "onHitBlock", at = @At(value = "INVOKE", target = "Lvazkii/botania/common/entity/ManaBurstEntity;onReceiverImpact(Lvazkii/botania/api/mana/ManaReceiver;)Z"))
+    private boolean onReceiverImpact(ManaBurstEntity instance, ManaReceiver collector, Operation<Boolean> original,
+            BlockHitResult hit) {
+        if (collector instanceof ManaP2PTunnelPart p2p) {
+            p2p.receiveManaFromBurst(instance, hit);
+            return false;
         }
+
+        return original.call(instance, collector);
     }
 }
